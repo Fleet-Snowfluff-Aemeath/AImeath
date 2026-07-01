8c6a204a379718e3ba0b485d526f17e1cca668da
bbrlbt <1973780613@qq.com>
2026-07-02 02:18:51 +0800

    fix:

--- module/core/src/pty_session.cpp
+++ module/core/src/pty_session.cpp
@@ -80,7 +80,8 @@ void PtySession::pushOutput(const std::string& text)
 void PtySession::write(const std::string& data)
 {
     if (m_master_fd >= 0 && !data.empty())
-        (void)::write(m_master_fd, data.data(), data.size());
+        auto _ = ::write(m_master_fd, data.data(), data.size());
+        (void)_;
 }
 
 void PtySession::resize(int rows, int cols)
